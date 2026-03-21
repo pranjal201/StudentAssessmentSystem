4107459866b1650476c069e65746dd868968be58
Pranjal Parmar <pranjalparmar2001@gmail.com>
2026-03-21 22:49:15 +0530

    test: Added Unit testing and Integration Testing solutions

---
+++ StudentAssessment/tests/StudentAssesment.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace StudentAssessment.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ StudentAssessment/tests/StudentAssessment.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace StudentAssessment.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
